59292ef6beb07d3fbe31dbb0e8079836fcf44988
nhinhist <s253votuyen@gmail.com>
2026-08-11 23:36:54 +0700

    Add files via upload

---
+++ PHP_ThucHanh/Buoi01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Bài PHP đầu tiên</title>
+</head>
+<body>
+
+<?php
+    $ten = "Thuỳ Linh!!!";
+    echo "Xin chào, " . $ten;
+?>
+
+</body>
+</html>
+
